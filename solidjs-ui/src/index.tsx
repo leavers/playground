5df3705fd4cace5bf3c809b8d68f992d89f76c99
Leavers <leavers930@gmail.com>
2023-12-14 20:35:15 +0800

    feat(solid-ui): init

--- solidjs-ui/src/index.tsx
+++ solidjs-ui/src/index.tsx
@@ -1,10 +1,11 @@
 /* @refresh reload */
-import { render } from "solid-js/web"
+import { render } from "solid-js/web";
 
-import "./index.css"
-import "virtual:uno.css"
-import App from "./App"
+import "@unocss/reset/normalize.css";
+import "virtual:uno.css";
+import "./index.css";
+import App from "./App";
 
-const root = document.getElementById("root")
+const root = document.getElementById("root");
 
-render(() => <App />, root!)
+render(() => <App />, root!);
